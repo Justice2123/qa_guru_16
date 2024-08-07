9888d851f5ad9cd6eea9a336db5199a1280195b1
Cherkasova Alyona <sentyao@yandex.ru>
2024-08-07 16:56:11 +0300

    add files

---
+++ src/test/java/models/CreateBodyModel.java
@@ -0,0 +1,22 @@
+package models;
+
+public class CreateBodyModel {
+
+    String name, job;
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/tests/ReqresInTest.java
@@ -0,0 +1,85 @@
+package tests;
+
+import models.CreateBodyModel;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+public class ReqresInTest extends TestBase {
+
+    @Test
+    void listDataNotEmptyTest() {
+        given()
+                .log()
+                .all()
+                .get("/unknown")
+                .then().log().body()
+                .statusCode(200)
+                .body("data", is(notNullValue()));
+    }
+
+
+    @Test
+    void listDataIdTest() {
+        given()
+                .log()
+                .all()
+                .get("/unknown")
+                .then().log().body()
+                .statusCode(200)
+                .body("data[0].id", is(1));
+    }
+
+
+    @Test
+    void unSuccessfulCreate415Test() {
+        given()
+                .log()
+                .uri()
+                .post("/users")
+                .then().log().status()
+                .statusCode(415);
+    }
+
+    @Test
+    void successfulCreateTest() {
+
+        //String authData = "{\"name\": \"morpheus\", \"job\": \"leader\"}";
+
+        CreateBodyModel authData = new CreateBodyModel();
+        authData.setName("morpheus");
+        authData.setJob("leader");
+
+        given()
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+
+    @Test
+    void singleUserNotFound() {
+        given()
+                .log()
+                .all()
+                .get("/users/23")
+                .then()
+                .log()
+                .all()
+                .statusCode(404);
+    }
+
+
+}
